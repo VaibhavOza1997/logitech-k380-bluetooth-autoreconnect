f3c03016a7f06f8ada0b1b62167e2df1d49b5c6b
Vaibhav Oza <37116174+VaibhavOza1997@users.noreply.github.com>
2025-09-20 22:15:21 -0400

    Update bt-reconnect.sh

--- bt-reconnect.sh
+++ bt-reconnect.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+# ---------------------------------------------------------
+#  Bluetooth Auto-Reconnect Script
+#  Author: Vaibhav Oza
+#  GitHub: https://github.com/vaibhavoza
+#  Created: 2025
+#  Description:
+#    - Monitors Bluetooth connection of Pebble Keys 2 K380
+#    - Auto-reconnects if the keyboard disconnects
+#    - Designed for RHEL 10 (but works on most Linux distros)
+# ---------------------------------------------------------
 
-KEYBOARD_MAC="DF:17:DC:EB:01:16"
+KEYBOARD_MAC="XX:XX:XX:XX:XX:XX" #Update MAC as per your bluetooth device
 
 /usr/bin/bluetoothctl info $KEYBOARD_MAC >/dev/null 2>&1
 
